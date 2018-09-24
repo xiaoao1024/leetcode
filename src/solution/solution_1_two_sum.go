e71388ee0b0a7d62a0ad385ef032f24a64156aae
xiaoao <xiaoao1024@126.com>
2018-09-24 23:37:37 +0800

    first solution

---
+++ src/solution/solution_1_two_sum.go
@@ -0,0 +1,12 @@
+package solution
+
+func TwoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{0, 0}
+}
