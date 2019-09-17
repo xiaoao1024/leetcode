bf780b96c3cd29edd14332991d6c5bf783d41f53
dirpc-sdk-builder <dirpc-sdk-builder@example.com>
2019-09-18 00:21:04 +0800

    调整目录结构

---
+++ solution/solution_299_get_hint.go
@@ -0,0 +1,27 @@
+package solution
+
+import "fmt"
+
+// 你正在和你的朋友玩 猜数字（Bulls and Cows）游戏：你写下一个数字让你的朋友猜。每次他猜测后，你给他一个提示，告诉他有多少位数字和确切位置都猜对了（称为“Bulls”, 公牛），有多少位数字猜对了但是位置不对（称为“Cows”, 奶牛）。你的朋友将会根据提示继续猜，直到猜出秘密数字。
+//
+// 请写出一个根据秘密数字和朋友的猜测数返回提示的函数，用 A 表示公牛，用 B 表示奶牛。
+//
+// 请注意秘密数字和朋友的猜测数都可能含有重复数字。
+
+func getHint(secret string, guess string) string {
+	var countA, countB int
+	length := len(secret)
+	for i := 0; i < length; i++ {
+		if guess[i] == secret[i] {
+			countA = countA + 1
+		} else {
+			for j := 0; j < length; j++ {
+				if guess[i] == secret[j] {
+					countB = countB + 1
+				}
+			}
+		}
+	}
+
+	return fmt.Sprintf("%vA%vB", countA, countB)
+}
